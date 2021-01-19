4e0975ede9115c3cd947b8f38dd63e32e88f5c70
Dap <32587372+daptheHuman@users.noreply.github.com>
2021-01-19 16:53:28 +0700

    Py Files
    uploaded first basic satpammer script

---
+++ satpammer.py
@@ -0,0 +1,22 @@
+# noinspection PyUnresolvedReferences
+import pyautogui
+import fileinput
+import time
+
+# Input File Script
+print("Enter Your File! *.txt*")
+filetxt = input()
+
+# Countdown!
+print("Open Your App Now")
+  time.sleep(5)
+
+# Attackto!!
+print("GG GEMING GAK GEISS??")
+print("https://github.com/daptheHuman")
+
+for line in fileinput.input(filetxt):
+    pyautogui.typewrite(line)
+    pyautogui.press("enter")
+pyautogui.typewrite("https://github.com/daptheHuman")
+pyautogui.press("enter")
